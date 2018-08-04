64d7d667cd51dbaf7a67bfb6ae8eb7dac81ef306
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-08-04 14:26:18 -0500

    MVP

--- src/init.js
+++ src/init.js
@@ -7,36 +7,42 @@
 
 // using jQuery and the object above, display the information as the appropriate button is clicked
 // 
-var fields = {
-  // Properties of our object.
-  firstName: "Dylan",
-  lastName: "Rutter",
-  jobTitle: "My job title is 'Resolutions Specialist'. Essentially we speak to all customers who demand supervisors from the front line agents. We aren't supervisors ourselves, just an escalated dept.",
-  homeOffice: "I work at the RPP location up North. It's a great office, and we finally got new electric desks!",
-  tellMeMore: "I am a big gamer. In my spare time I love modding arcade sticks and mostly play Street Fighter",
-  // click = function buttonClick() {
-  //   $(this.fields).onclick
-  // }
-
-};
-
-$('#firstName').click(function () {
-  alert("My first name is Dylan!")
-});
-
-$('#lastName').click(function () {
-  alert("My last name is Rutter! My Mom always said 'it's Rutter like Butter' when people ask the spelling.")
-});
+var fields = new Object();
+// Properties of our object.
+fields.firstName = "My first name is Dylan!";
+fields.lastName = "My last name is Rutter! My Mom always said 'it's Rutter like Butter' when people ask the spelling.";
+fields.jobTitle = "My job title is 'Resolutions Specialist'. Essentially we speak to all customers who demand supervisors from the front line agents. We aren't supervisors ourselves, just an escalated dept.";
+fields.homeOffice = "I work at the RPP location up North. It's a great office, and we finally got new electric desks!";
+fields.tellMeMore = "I am a big gamer. In my spare time I love modding arcade sticks and mostly play Street Fighter";
+fields.checkMessage = function () {
+  console.log(this.firstName);
 
 
-$('#jobTitle').click(function () {
-  alert("My job title is 'Resolutions Specialist'. Essentially we speak to all customers who demand supervisors from the front line agents. We aren't supervisors ourselves, just an escalated dept.")
-});
 
-$('#homeOffice').click(function () {
-  alert("I work at the RPP location up North. It's a great office, and we finally got new electric desks!")
-});
-
-$('#tellMeMore').click(function () {
-  alert("I am a big gamer. In my spare time I love modding arcade sticks and mostly play Street Fighter")
-});
+  $('#firstName').click(() => {
+    console.log(this);
+    console.log(this.firstName);
+    alert(this.firstName)
+  });
+  $('#lastName').click(() => {
+    console.log(this);
+    console.log(this.lastName);
+    alert(this.lastName)
+  });
+  $('#jobTitle').click(() => {
+    console.log(this);
+    console.log(this.jobTitle);
+    alert(this.jobTitle)
+  });
+  $('#homeOffice').click(() => {
+    console.log(this);
+    console.log(this.homeOffice);
+    alert(this.homeOffice)
+  });
+  $('#tellMeMore').click(() => {
+    console.log(this);
+    console.log(this.tellMeMore);
+    alert(this.tellMeMore)
+  });
+}
+fields.checkMessage();
